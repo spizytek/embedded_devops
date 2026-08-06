ca1dfd2a1d33e9ff2a28ccd5cc410d710375bc22
spizytek <titration97@gmail.com>
2026-08-06 12:09:24 +0100

    added comment for watch dog

--- ci_cd_demo/Debug/Src/main.d
+++ ci_cd_demo/Debug/Src/main.d
@@ -6,7 +6,8 @@ Src/main.o: ../Src/main.c \
  /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Include/cmsis_compiler.h \
  /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Include/cmsis_gcc.h \
  /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Include/mpu_armv7.h \
- /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+ /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ ../Inc/health.h
 /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
 /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f429xx.h:
 /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Include/core_cm4.h:
@@ -15,3 +16,4 @@ Src/main.o: ../Src/main.c \
 /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Include/cmsis_gcc.h:
 /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Include/mpu_armv7.h:
 /Users/user/Developer/embedded/stm32/CI_CD/ci_cd_demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+../Inc/health.h:
