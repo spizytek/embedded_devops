ba316e895667ccd28f57977b6afd2fba5c2e1105
spizytek <titration97@gmail.com>
2026-09-12 15:49:11 +0200

    Had to remove Release folder from the ignore folder

---
+++ ci_cd_demo/Release/Src/subdir.mk
@@ -0,0 +1,51 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Src/adc.c \
+../Src/bsp.c \
+../Src/fpu.c \
+../Src/health.c \
+../Src/main.c \
+../Src/syscalls.c \
+../Src/sysmem.c \
+../Src/timebase.c \
+../Src/uart.c 
+
+OBJS += \
+./Src/adc.o \
+./Src/bsp.o \
+./Src/fpu.o \
+./Src/health.o \
+./Src/main.o \
+./Src/syscalls.o \
+./Src/sysmem.o \
+./Src/timebase.o \
+./Src/uart.o 
+
+C_DEPS += \
+./Src/adc.d \
+./Src/bsp.d \
+./Src/fpu.d \
+./Src/health.d \
+./Src/main.d \
+./Src/syscalls.d \
+./Src/sysmem.d \
+./Src/timebase.d \
+./Src/uart.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -DSTM32F429I_DISC1 -DSTM32 -DSTM32F429ZITx -DSTM32F4 -c -I../Inc -I../chip_headers/CMSIS/Include -I../chip_headers/CMSIS/Device/ST/STM32F4xx/Include -Os -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Src
+
+clean-Src:
+	-$(RM) ./Src/adc.cyclo ./Src/adc.d ./Src/adc.o ./Src/adc.su ./Src/bsp.cyclo ./Src/bsp.d ./Src/bsp.o ./Src/bsp.su ./Src/fpu.cyclo ./Src/fpu.d ./Src/fpu.o ./Src/fpu.su ./Src/health.cyclo ./Src/health.d ./Src/health.o ./Src/health.su ./Src/main.cyclo ./Src/main.d ./Src/main.o ./Src/main.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su ./Src/timebase.cyclo ./Src/timebase.d ./Src/timebase.o ./Src/timebase.su ./Src/uart.cyclo ./Src/uart.d ./Src/uart.o ./Src/uart.su
+
+.PHONY: clean-Src
+
